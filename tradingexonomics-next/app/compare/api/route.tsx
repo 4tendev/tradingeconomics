7da6d2e75b6b9f1297aa1ded62cc27f327ae05a8
4tendev <4tenprog@gmail.com>
2024-11-07 17:04:12 +0330

    now use one fetch for all data

--- tradingexonomics-next/app/compare/api/route.tsx
+++ tradingexonomics-next/app/compare/api/route.tsx
@@ -1,29 +1,20 @@
-import { TIMEOUT } from "dns";
-
 export async function POST(request: Request) {
   const userRequest = await request.json();
   const indicator = userRequest.indicator;
+  const countries = userRequest.countries.join(",")  ;
 
-  const contories = userRequest.countries;
-
-  const data: any = [];
-
-  for (let index = 0; index < contories.length; index++) {
-    const country = contories[index];
-    const res = await fetch(
-      `https://api.tradingeconomics.com/historical/country/${country}/indicator/${indicator}?c=e0721bcb6806404:z9606uviey8lwwm&f=json`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    const responseText = await res.text();
-    data[index] = responseText;
-  }
- 
-  return new Response(data, {
+  const res = await fetch(
+    `https://api.tradingeconomics.com/historical/country/${countries}/indicator/${indicator}?c=e0721bcb6806404:fsxzeyve8e7i82z&f=json`,
+    {
+      method: "GET",
+    }
+  );
+  const responseJson = await res.json();
+  return new Response(JSON.stringify(responseJson), { // Serialize data to JSON
     status: 200,
+    headers: {
+      
+      "Content-Type": "application/json", // Set content-type header to JSON
+    },
   });
 }
